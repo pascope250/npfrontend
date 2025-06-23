19134bb9e68e6db453e26d9226c710a13100930a
name <pascope250@gmail.com>
2025-06-23 11:15:02 +0200

    change links

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,5 +1,4 @@
-import Link from 'next/link';
-
+'use client';
 interface MenuCardProps {
   title: string;
   description: string;
@@ -9,21 +8,18 @@ interface MenuCardProps {
 }
 
 const MenuCard = ({ title, description, href, icon, bgColor }: MenuCardProps) => {
-  // open new tab
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    window.open(href, '_blank');
-  };
+  
   return (
-    <Link href={href} onClick={handleClick}>
-      <div className={`${bgColor} rounded-xl shadow-lg overflow-hidden transform transition-all hover:scale-105 hover:shadow-xl cursor-pointer h-full`}>
+
+      <div className={`${bgColor} rounded-xl shadow-lg overflow-hidden transform transition-all hover:scale-105 hover:shadow-xl cursor-pointer h-full`}
+      onClick={() => window.open(href, '_blank')}
+      >
         <div className="p-6 flex flex-col items-center text-center">
           <div className="mb-4 text-4xl animate-float">{icon}</div>
           <h3 className="text-2xl font-bold text-white mb-2">{title}</h3>
           <p className="text-white opacity-90">{description}</p>
         </div>
       </div>
-    </Link>
   );
 };
 
