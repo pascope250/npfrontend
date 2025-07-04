4e329fc21c7b26297df2dbba04322fabaa1b47c0
name <pascope250@gmail.com>
2025-07-04 19:39:20 +0200

    change ads loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,7 +105,7 @@ export default function RootLayout({
           strategy="afterInteractive"
           crossOrigin="anonymous"
         />
-        <Script
+        {/* <Script
           id="adsbygoogle-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
@@ -116,7 +116,7 @@ export default function RootLayout({
       });
     `,
           }}
-        />
+        /> */}
 
         <Script
           id="disable-context-menu"
